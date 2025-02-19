c18bba31253532ed9d5cf70286b226a988339c37
Ghanshyam Vaja <ghanshyamvaja11@gmail.com>
2025-02-19 08:54:14 +0530

    some changes in urls.pys

--- certificate/urls.py
+++ certificate/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('intern/verify/',
+    path('intern/verify',
          verify_certificate, name='verify_certificate'),
-    path('intern/verify/<str:certificate_code>/', verify_certificate_by_link, name='verify_certificate_by_link'),
+    path('intern/verify/<str:certificate_code>', verify_certificate_by_link, name='verify_certificate_by_link'),
     path("save-certificate/",
          save_certificate, name="save_certificate"),
 ]
--- entry/urls.py
+++ entry/urls.py
@@ -3,14 +3,14 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('about/', about,
+    path('about', about,
          name='about'),  # Redirect after login
-    path('servicies/', services, name='servicies'),
-    path('contact/', contact, name='contact'),
-    path('terms-and-conditions/', T_and_C, name='T&C'),
-    path('privacy-policy/', privacy_policy, name="privacy-policy"),
-    path('cancellation-refund_policies/', cancellation_refund_policies,
+    path('servicies', services, name='servicies'),
+    path('contact', contact, name='contact'),
+    path('terms-and-conditions', T_and_C, name='T&C'),
+    path('privacy-policy', privacy_policy, name="privacy-policy"),
+    path('cancellation-refund_policies', cancellation_refund_policies,
          name='cancellation_refund_policies'),
 
-    path('page-not-found/', error_404_view, name='error_404_view'),
+    path('page-not-found', error_404_view, name='error_404_view'),
 ]
