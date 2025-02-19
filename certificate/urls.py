417e919c6fcc94f12292a31b665a6fdd7d677c4a
Ghanshyam Vaja <ghanshyamvaja11@gmail.com>
2025-02-19 09:13:13 +0530

    Some Changes in footers

--- certificate/urls.py
+++ certificate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('intern/verify',
+    path('intern/verify/',
          verify_certificate, name='verify_certificate'),
     path('intern/verify/<str:certificate_code>', verify_certificate_by_link, name='verify_certificate_by_link'),
     path("save-certificate/",
--- entry/urls.py
+++ entry/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('servicies', services, name='servicies'),
     path('contact', contact, name='contact'),
     path('terms-and-conditions', T_and_C, name='T&C'),
-    path('privacy-policy', privacy_policy, name="privacy-policy"),
-    path('cancellation-refund_policies', cancellation_refund_policies,
+    path('privacy-policy', privacy_policy, name="privacy_policy"),
+    path('cancellation-refund-policies', cancellation_refund_policies,
          name='cancellation_refund_policies'),
 
-    path('page-not-found', error_404_view, name='error_404_view'),
+    path('page-not-found/', error_404_view, name='error_404_view'),
 ]
